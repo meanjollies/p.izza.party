0ab256e4575ca58f38b867b405c3afd9c59de490
meanjollies <meanjollies@users.noreply.github.com>
2020-01-22 18:59:06 -0500

    Create backend so the state file can be stored remotely

---
+++ backend.tf
@@ -0,0 +1,9 @@
+# State file is stored remotely
+terraform {
+  backend "s3" {
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
+}
